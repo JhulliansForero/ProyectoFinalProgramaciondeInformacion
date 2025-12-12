c5199666e255a7835d84294070f6b115fb36952f
jhullians <jhulliansforero@gmail.com>
2025-12-12 07:58:05 -0500

    Configuracion del admin y documentacion

--- vistas/admin.py
+++ vistas/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 
-from .models import Practica
+from .models import Practica, Obra, Capitulo
 
 @admin.register(Practica)
 class PracticaAdmin(admin.ModelAdmin):
-    list_display = ("id", "username", "password")  # columnas que quieres ver
-    search_fields = ("username",)                  # barra de búsqueda
+    list_display = ("id", "username", "email", "password")  # Agregado email y password
+    search_fields = ("username", "email")
     list_filter = ("username",)
+
+@admin.register(Obra)
+class ObraAdmin(admin.ModelAdmin):
+    list_display = ("id", "titulo", "autor", "categoria")
+    search_fields = ("titulo", "autor__username")
+
+@admin.register(Capitulo)
+class CapituloAdmin(admin.ModelAdmin):
+    list_display = ("id", "titulo", "obra", "fecha_creacion")
+    search_fields = ("titulo", "obra__titulo")
